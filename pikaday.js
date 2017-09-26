41b6e33be097b9e718927c8da47c4947cdb26a38
Chris Seufert <chris@seufert.id.au>
2017-09-26 15:48:13 +1000

    Add option to allow adding custom classes to days in calendars

--- pikaday.js
+++ pikaday.js
@@ -341,6 +341,8 @@
         if (opts.isEndRange) {
             arr.push('is-endrange');
         }
+        if (opts.dayClass)
+            opts.dayClass(opts, arr);
         return '<td data-day="' + opts.day + '" class="' + arr.join(' ') + '" aria-selected="' + ariaSelected + '">' +
                  '<button class="pika-button pika-day" type="button" ' +
                     'data-pika-year="' + opts.year + '" data-pika-month="' + opts.month + '" data-pika-day="' + opts.day + '">' +
